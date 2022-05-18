41e7b392b255c237be3ee90c3138515f1b2132eb
JordHemsworth <jordanhemsworth+1@gmail.com>
2022-05-18 17:55:16 +0100

    Show past guesses

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
-export default function Row() {
+export default function Row( {guess}) {
+
+    if (guess) {
+        return (
+            <div className='row past'>
+                {guess.map((l, i) => (                                  /* If the guess has a value, return a row with letters in each square */
+                    <div key={i} className={l.color}> {l.key} </div>
+                ))}
+            </div>
+        )
+    }
+
   return (
-    <div class="row">
+    <div className="row">
         <div></div>
         <div></div>
         <div></div>
