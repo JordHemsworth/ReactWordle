d6b887e135d2dc38fdef3e9fd2d917f31a8fdcc1
JordHemsworth <jordanhemsworth+1@gmail.com>
2022-05-18 12:25:17 +0100

    Submit guesses with some validaiton

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -5,13 +5,13 @@ const useWordle = (solution) => {
     const [turn, setTurn] = useState(0) 
     const [currentGuess, setCurrentGuess] = useState('')
     const [guesses, setGuesses] = useState([]) // each guess is an array
-    const [history, setHistory] = useState([]) // each guess is a string
+    const [history, setHistory] = useState(['hello']) // each guess is a string
     const [isCorrect, setIsCorrect] = useState(false)
 
 // format a guess into an array of letter objects 
 // e.g. [{key: 'a', color: 'yellow'}]
 const formatGuess = () => {
-    
+    console.log('Formatting the guess -', currentGuess)
 }
 
 // add a new guess to the guesses state
@@ -25,6 +25,29 @@ const addNewGuess = () => {
 // if user presses enter, add the new guess
 const handleKeyup = ({key}) => {
     
+    if (key === 'Enter') {
+        // Only add if guess turn < 5
+        if (turn > 5) {
+            console.log('Used all guesses')
+            return
+        }
+
+        // Do not allow duplicate words.
+
+        if (history.includes(currentGuess)) {
+
+            console.log('Already tried that word')
+            return
+        }
+        // Check word is 5 chars long
+
+        if (currentGuess.length !== 5) {
+            console.log('Word must be 5 chars long')
+            return
+        } 
+        formatGuess()       /* Call function only if all conditions are valid */
+    }
+
     if (key === 'Backspace') {
         setCurrentGuess((prev) => {             /* Pass function to get previous state value, then remove last character using slice */
             return prev.slice(0, -1)
@@ -39,6 +62,8 @@ const handleKeyup = ({key}) => {
             })
         }
     }
+
+    /* If currentGuess.length == 5, then setGuess, only add if turn < 5, if Guess matches solution, then isCorrect returns true, else setHistory for invalid guess */
 
 }
 
