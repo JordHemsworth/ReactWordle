2c64e2994f0e47f29a53b31d03c6b290c75e62d6
JordHemsworth <jordanhemsworth+1@gmail.com>
2022-05-20 13:54:07 +0100

    Added replay option

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,13 +8,15 @@ export default function Modal({isCorrect, turn, solution}) {
                 <h1> You Win! </h1>
                 <p className='solution'>{solution.toUpperCase()}</p>
                 <p> You found the correct answer in {turn} guesses! </p>
+                <a href="http://localhost:3000/" className='replay'>Play Again!</a>
             </div>
         )}
         {!isCorrect && (
             <div>
                 <h1> You Lose! </h1>
-                <p className='solution'>The correct answer was - {solution.toUpperCase()}()</p>
+                <p className='solution'>The correct answer was - {solution.toUpperCase()}</p>
                 <p> Better luck next time. </p>
+                <a href="http://localhost:3000/" className='replay'>Play Again!</a>
             </div>
         )}
     </div>
